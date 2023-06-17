79e3d97e59748a8c4a228ec90f50ded17a5dec2f
JOOMYEONGGYUN <106478922+JOOMYEONGGYUN@users.noreply.github.com>
2023-06-17 16:26:25 +0900

    enemy_1n

---
+++ enemy_1n.h
@@ -0,0 +1,12 @@
+#ifndef ENEMY_1N_H
+#define ENEMY_1N_H
+#include "enemy.h"
+
+class enemy_1n: public enemy
+{
+    public:
+    enemy_1n(int y_value, int x_value, int frame_value):enemy(y_value, x_value, frame_value, 10, 0, 1,'n') {}
+    private:
+};
+
+#endif
