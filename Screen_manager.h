7f0f38706bb38ca1974440787efc8f196c2f3c5d
JOOMYEONGGYUN <106478922+JOOMYEONGGYUN@users.noreply.github.com>
2023-06-17 16:46:12 +0900

    screen manager

--- Screen_manager.h
+++ Screen_manager.h
@@ -12,6 +12,9 @@
 #include <chrono>
 #include <vector>
 #include "My_plane.h"
+#include "unit.h"
+#include "enemy.h"
+#include "item.h"
 
 using namespace std;
 
@@ -20,6 +23,10 @@ class Screen_manager{
     public:
         int frame_length=100000; //each frame's length == 0.1sec(10,000 microseconds)
         int curr_frame=0; //1frame=0.1sec
+        int count = 0;
+        vector<enemy*> enemies;
+        vector<item*> items;
+        vector<Enemy_bullet> enemy_bullets;
 
         int y, x;
         int width=60;
@@ -29,9 +36,15 @@ class Screen_manager{
 
         void print(int ascii);
         void print();
+        void print_end();
         void print_share();
         void render();
         char board[30][60]={'a'};
         My_plane my_plane = My_plane(height-2, 1, 0);
+        
+        void create_event();
+        void interaction();
+        bool isFinish(bool all_enemy);
+        char board[30][60]={'a'};
 };
 #endif
